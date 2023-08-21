ecbb594205e42a57eead07570ce86d42df347ce5
Vidyadhar Angadiyavar <vidyadhar.angadiyavar@sap.com>
2023-08-21 10:11:58 -0700

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import {MultiComboExComponent} from "./multi-combo-ex/multi-combo-ex.component";
 import { AppComponent } from './app.component';
+import {PlatformMultiComboboxModule} from "@fundamental-ngx/platform";
 
 
 
 @NgModule({
   declarations: [MultiComboExComponent, AppComponent],
   imports: [
-    CommonModule, BrowserModule
+    CommonModule, BrowserModule,
+    PlatformMultiComboboxModule
   ],
   bootstrap: [AppComponent]
 })
